2dbcb1fb4709ca48d8d580aa79c694d251fcb44c
Raks-hith <97620722+Raks-hith@users.noreply.github.com>
2022-12-27 22:34:54 +0530

    Update user_proj_example.v

--- verilog/rtl/user_proj_example.v
+++ verilog/rtl/user_proj_example.v
@@ -36,129 +36,196 @@
  */
 
 module user_proj_example (
-`ifdef USE_POWER_PINS
-    inout vccd1,	// User area 1 1.8V supply
-    inout vssd1,	// User area 1 digital ground
-`endif
 
-    // Wishbone Slave ports (WB MI A)
-    input wb_clk_i,
-    input wb_rst_i,
-    input wbs_stb_i,
-    input wbs_cyc_i,
-    input wbs_we_i,
-    input [3:0] wbs_sel_i,
-    input [31:0] wbs_dat_i,
-    input [31:0] wbs_adr_i,
-    output wbs_ack_o,
-    output [31:0] wbs_dat_o,
+input clk, reset , input  [1:0]z01, z02, z03, z04, z11, z12, z13, z14, output reg  [19:0] alpha);
 
-    // Logic Analyzer Signals
-    input  [127:0] la_data_in,
-    output [127:0] la_data_out,
-    input  [127:0] la_oenb,
+reg  [200:0]temp;
+reg  [19:0] e;
+reg  [2:0] x1;
+reg  [2:0] x0;
+reg  [19:0] a;
+reg  [19:0] b;
+reg  [19:0] r;
+reg  [19:0] fa;
+reg  [19:0] fb;
+reg  [19:0] fc;
+reg  [19:0]z;
+//reg  [19:0]y;
+reg  [2:0]c[8:0];
+always@(posedge clk or posedge reset)
+begin
+if(reset==1'b1)
+begin
+a= 20'b00000011110000000000;
+b= 20'b00000101000000000000;
 
-    // IOs
-    input  [`MPRJ_IO_PADS-1:0] io_in,
-    output [`MPRJ_IO_PADS-1:0] io_out,
-    output [`MPRJ_IO_PADS-1:0] io_oeb,
+end
 
-    // IRQ
-    output [2:0] irq
-);
-    wire clk;
-    wire rst;
+else
+begin
 
-    wire [`MPRJ_IO_PADS-1:0] io_in;
-    wire [`MPRJ_IO_PADS-1:0] io_out;
-    wire [`MPRJ_IO_PADS-1:0] io_oeb;
+e = 20'b00000000000000000001;
+c[0]= 3'b0;
+x1= {z11[1], z11};
+x0= {z01[1], z01};
+c[1]= x1- x0;
+x1= {z12[1], z12};
+x0= {z02[1], z02};
+c[2]= x1- x0;
+x1= {z13[1], z13};
+x0= {z03[1], z03};
+c[3]= x1- x0;
+x1= {z14[1], z14};
+x0= {z04[1], z04};
+c[4]= x1- x0;
 
-    wire [31:0] rdata; 
-    wire [31:0] wdata;
-    wire [BITS-1:0] count;
 
-    wire valid;
-    wire [3:0] wstrb;
-    wire [31:0] la_write;
 
-    // WB MI A
-    assign valid = wbs_cyc_i && wbs_stb_i; 
-    assign wstrb = wbs_sel_i & {4{wbs_we_i}};
-    assign wbs_dat_o = rdata;
-    assign wdata = wbs_dat_i;
 
-    // IO
-    assign io_out = count;
-    assign io_oeb = {(`MPRJ_IO_PADS-1){rst}};
+fa= 20'b0;
+fb= 20'b0;
+fc= 20'b0;
+r= a+b;
+if(r[19]==1)
+r= {1'b1, r>>1};
+else
+r= r>>1;
 
-    // IRQ
-    assign irq = 3'b000;	// Unused
+fa = s(c[0],c[1],c[2],c[3],c[4],a);
+fb = s(c[0],c[1],c[2],c[3],c[4],b);
+fc = s(c[0],c[1],c[2],c[3],c[4],r);
 
-    // LA
-    assign la_data_out = {{(127-BITS){1'b0}}, count};
-    // Assuming LA probes [63:32] are for controlling the count register  
-    assign la_write = ~la_oenb[63:32] & ~{BITS{valid}};
-    // Assuming LA probes [65:64] are for controlling the count clk & reset  
-    assign clk = (~la_oenb[64]) ? la_data_in[64]: wb_clk_i;
-    assign rst = (~la_oenb[65]) ? la_data_in[65]: wb_rst_i;
-
-   counter #(
-        .BITS(BITS)
-    ) counter(
-        .clk(clk),
-        .reset(rst),
-        .ready(wbs_ack_o),
-        .valid(valid),
-        .rdata(rdata),
-        .wdata(wbs_dat_i),
-        .wstrb(wstrb),
-        .la_write(la_write),
-        .la_input(la_data_in[63:32]),
-        .count(count)
-    );
-    
-
-endmodule
-
-module counter #(
-    parameter BITS = 32
-)(
-    input clk,
-    input reset,
-    input valid,
-    input [3:0] wstrb,
-    input [BITS-1:0] wdata,
-    input [BITS-1:0] la_write,
-    input [BITS-1:0] la_input,
-    output ready,
-    output [BITS-1:0] rdata,
-    output [BITS-1:0] count
-);
-    reg ready;
-    reg [BITS-1:0] count;
-    reg [BITS-1:0] rdata;
-
-    always @(posedge clk) begin
-        if (reset) begin
-            count <= 0;
-            ready <= 0;
-        end else begin
-            ready <= 1'b0;
-            if (~|la_write) begin
-                count <= count + 1;
-            end
-            if (valid && !ready) begin
-                ready <= 1'b1;
-                rdata <= count;
-                if (wstrb[0]) count[7:0]   <= wdata[7:0];
-                if (wstrb[1]) count[15:8]  <= wdata[15:8];
-                if (wstrb[2]) count[23:16] <= wdata[23:16];
-                if (wstrb[3]) count[31:24] <= wdata[31:24];
-            end else if (|la_write) begin
-                count <= la_write & la_input;
-            end
-        end
+if (abso(fc)>e)
+    begin
+    if (((fa[19]==1) &&(fc[19]==0))|| ((fa[19]==0) &&(fc[19]==1)))
+          b =r;
+    else
+          a =r;
+          r= a+b;
+          r= r>>1;
+    fc = s(c[0],c[1],c[2],c[3],c[4],r);
+   
+   
+    temp= 201'd32768;
+          temp= temp<<15;
+        if(r[19]==1)
+       
+        temp= temp/(-r);
+        else
+        temp= temp/r;
+        alpha= temp[19:0];
+       
+        z = s(0,z01,z02,z03,z04,r);
+        temp= 201'd8192;
+        temp= temp<<15;
+        
     end
+   
+end
+end
+
+
+function [19:0] s;
+    input [2:0] c0;
+    input [2:0] c1;
+    input [2:0] c2;
+    input [2:0] c3;
+    input [2:0] c4;
+  
+    input [19:0] val;
+    reg [19:0] f;
+    reg [19:0] y;
+    reg [200:0] temp_pow;
+    begin
+    f=19'b0;
+    temp_pow= val;
+    y= temp_pow[19:0];
+   
+    if(c1 ==3'b001);
+    else if(c1 ==3'b010)
+    y = y<<1;
+    else if(c1 ==3'b000)
+    y=0;
+    else if(c1 ==3'b111)
+    y = -y;
+    else //if(x == 3'b110)
+    begin
+    y= -y;
+    y= y<<1;
+    end
+    f= f+y;
+   
+    //f= c1*temp_pow;
+
+    temp_pow= temp_pow*val;
+    temp_pow = temp_pow>>15;
+    y= temp_pow[19:0];
+    if(c2 ==3'b001);
+    else if(c2 ==3'b010)
+    y = y<<1;
+    else if(c2 ==3'b000)
+    y=0;
+    else if(c2 ==3'b111)
+    y = -y;
+    else //if(x == 3'b110)
+    begin
+    y= -y;
+    y= y<<1;
+    end
+    f= f+y;
+    //f= f+ c2*temp_pow;
+ 
+    temp_pow= temp_pow*val;
+    temp_pow = temp_pow>>15;
+    y= temp_pow[19:0];
+    if(c3 ==3'b001);
+        else if(c3 ==3'b010)
+        y = y<<1;
+        else if(c3 ==3'b000)
+        y=0;
+        else if(c3 ==3'b111)
+        y = -y;
+        else //if(x == 3'b110)
+        begin
+        y= -y;
+        y= y<<1;
+        end
+        f= f+y;
+    //f= f+ c3*temp_pow;
+
+    temp_pow= temp_pow*val;
+    temp_pow = temp_pow>>15;
+    y= temp_pow[19:0];
+    if(c4 ==3'b001);
+        else if(c4 ==3'b010)
+        y = y<<1;
+        else if(c4 ==3'b000)
+        y=0;
+        else if(c4 ==3'b111)
+        y = -y;
+        else //if(x == 3'b110)
+        begin
+        y= -y;
+        y= y<<1;
+        end
+        f= f+y;
+    //f= f+ c4*temp_pow;
+
+   
+   
+   s= f;
+ end      
+endfunction
+
+function [19:0] abso;
+    input [19:0] fc;
+    if (fc[19]==1)
+        abso = -(fc);
+    else
+        abso = fc;
+    endfunction
+
+
 
 endmodule
 `default_nettype wire
